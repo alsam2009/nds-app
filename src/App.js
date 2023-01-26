252e114691d27e10ddfc16ae40b9cdf2216f54fe
alsam2009 <alsam2009@gmail.com>
2023-01-26 20:02:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="./logo.png" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className="text-red-500">News </span>
+          <span className="text-orange-500">Delivery </span>
+          <span className="text-lime-500">System</span>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://t.me/c/1831583082/189"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        ><br />
+          see us on Telegram
         </a>
       </header>
     </div>
