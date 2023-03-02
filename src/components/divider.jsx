533d7cd210f59581fa02c078b1386cb932d62776
Den <alsam2009@gmail.com>
2023-03-02 18:38:42 +0300

    Добавил компонент Divider

---
+++ src/components/divider.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Divider = ({ name, color }) => {
+  const paramDivider = `w-auto h-6 mx-8 mb-5 border-b-2 border-${color} items-center text-base-200  divider`
+  console.log(paramDivider)
+  return (
+    <div
+      className={paramDivider}
+      >
+      <span className={`px-2 py-0.5 bg-${color}`}>{name}</span>
+      </div>
+  )
+}
+
+export default Divider
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CardList from "./card/cardList";
+import Divider from "./divider";
 
 const Main = () => {
   return (
     <main className="pt-8 dark:bg-base-500">
+      <Divider name="Важное" color="color-150"/>
+      <Divider name="Все новости" color="color-550"/>
       <CardList />
     </main>
   );
