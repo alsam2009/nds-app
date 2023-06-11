efc02f325a9a7b5eaef7fac5614a6e737d52c722
Den <alsam2009@gmail.com>
2023-06-11 21:53:44 +0300

    Избранное с localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,39 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { reducer as favouriteReducer } from './slices/favouriteSlice'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { reducer as favouriteReducer } from './slices/favouriteSlice';
 
-const reducers = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
   favourites: favouriteReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: true
 })
+
+export const persistor = persistStore(store)
 
 export default store
